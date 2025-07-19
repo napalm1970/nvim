9b186bb6b111c13e272e10dc86dcc41bb9a9c8a8
napalm1970 <napalm1970@gmail.com>
2025-07-19 14:03:51 +0300

    Create new config.

--- lua/hopconfig.lua
+++
@@ -1,9 +0,0 @@
--- place this in one of your configuration file(s)
-local hop = require("hop")
-local directions = require("hop.hint").HintDirection
-vim.keymap.set("", "f", function()
-	hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = false })
-end, { remap = true })
-vim.keymap.set("", "F", function()
-	hop.hint_lines({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-end, { remap = true })
--- lua/lsp_linesconf.lua
+++
@@ -1,7 +0,0 @@
-require("lsp_lines").setup()
-
-vim.diagnostic.config({
-	virtual_text = false,
-})
-
-vim.keymap.set("", "<Leader>ll", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
